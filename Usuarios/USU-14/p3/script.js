e0bfe4ef6e0ac62e9c80739f1025daccaa99cf50
Silvia Redondo <silviarlaredo@gmail.com>
2024-06-19 08:50:04 +0200

    p3
    subo practica 3

---
+++ Usuarios/USU-14/p3/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Aquí puedes añadir cualquier funcionalidad adicional con JavaScript
+    console.log("Ficha de usuario cargada");
+});
