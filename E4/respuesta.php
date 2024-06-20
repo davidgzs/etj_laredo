e54917a069a9d2ae137e33e6b0861d1cea771f8b
Jonathan Cantero Guardado <jonathancanterog@gmail.com>
2024-06-20 11:55:46 +0200

    E4-SerieFav

---
+++ E4/respuesta.php
@@ -0,0 +1,27 @@
+<?php
+// Obtiene el usuario seleccionado del formulario
+$usuario = isset($_GET['usuario']) ? intval($_GET['usuario']) : 0;
+
+// Array de series favoritas por usuario
+$series_favoritas = [
+    1 => "Juego de Tronos",
+    2 => "Peaky Blinders"
+];
+
+// Determina la serie favorita del usuario
+$serie_favorita = isset($series_favoritas[$usuario]) ? $series_favoritas[$usuario] : "No especificada";
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Serie Favorita</title>
+</head>
+<body>
+    <h1>Información del Usuario</h1>
+    <p>Al usuario <?php echo $usuario; ?> le gusta la serie: <?php echo $serie_favorita; ?>.</p>
+    <button onclick="window.location.href='index.html'">Volver</button>
+</body>
+</html>
